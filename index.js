28c41500067b8759f2b99d25533077cf32817f32
gmustafa13 <gmmansoori.13@gmail.com>
2019-09-08 21:53:25 +0530

    contactlist

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.static(__dirname +'/public'));
 
+app.get('/contactlist',function(req,res){
+ var contactlist=[
+    {
+        name:"Gulam Mustafa",
+        email:"gmustafa.mansoori@gmail.com",
+        contact:"9987780051"
+    },
+    {
+        name:"Gulam Ali",
+        email:"gmustafa.mansoori@gmail.com",
+        contact:"9890806960"
+    },
+    {
+        name:"Khalid",
+        email:"mohdkhalid@gmail.com",
+        contact:"9594850138"
+    }
+        ]
+        res.json(contactlist)
+})
+
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/backendPractice",
--- public/controller/contactListCtrl.js
+++ public/controller/contactListCtrl.js
@@ -1,3 +1,23 @@
-function myContact(){
-    console.log("from contect ListController")
-}
+angular.module("myModule",[]).controller('myContact',function($scope,$http){
+
+    $http.get('/contactlist').success(function(response){
+        $scope.contactlist=response
+    })
+//     $scope.contactlist=[
+// {
+//     name:"Gulam Mustafa",
+//     email:"gmustafa.mansoori@gmail.com",
+//     contact:"9987780051"
+// },
+// {
+//     name:"Gulam Ali",
+//     email:"gmustafa.mansoori@gmail.com",
+//     contact:"9890806960"
+// },
+// {
+//     name:"Khalid",
+//     email:"mohdkhalid@gmail.com",
+//     contact:"9594850138"
+// }
+//     ]
+})
